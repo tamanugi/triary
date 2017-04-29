8dc56932b407aa2dfd0ef6abb28c6f75947207a4
tamanugi <tamanugi@gmail.com>
2017-04-29 16:12:38 +0900

    アプリケーション起動時にStateAgentモジュールを開始させる

--- lib/triary.ex
+++ lib/triary.ex
@@ -14,6 +14,7 @@ defmodule Triary do
       supervisor(Triary.Endpoint, []),
       # Start your own worker by calling: Triary.Worker.start_link(arg1, arg2, arg3)
       # worker(Triary.Worker, [arg1, arg2, arg3]),
+      supervisor(Triary.StateAgent, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
