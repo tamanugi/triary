9485c812e57ea348b3849947ce18c0175e34089c
tamanugi <tamanugi@gmail.com>
2017-04-29 15:54:53 +0900

    外部から叩ける用にルーティングを修正

--- web/router.ex
+++ web/router.ex
@@ -17,11 +17,12 @@ defmodule Triary.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    resources "/linebot", LinebotController
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", Triary do
-  #   pipe_through :api
-  # end
+  scope "/api", Triary do
+    pipe_through :api
+
+    resources "/linebot", LinebotController
+  end
 end
