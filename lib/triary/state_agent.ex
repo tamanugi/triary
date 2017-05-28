f43834f41fdfd305baa8d298d329defebf7aa85e
tamanugi <tamanugi@gmail.com>
2017-05-28 23:18:46 +0900

    state_agent.exをDiary的な使い方ではなく、ステートマシンとして動かすように修正

--- lib/triary/state_agent.ex
+++ lib/triary/state_agent.ex
@@ -1,20 +1,34 @@
 defmodule Triary.StateAgent do
-  def start_link do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
+
+  alias Triary.LinebotApi
+
+  def start_link(name) do
+    Agent.start_link(fn -> :worst end, name: name)
   end
 
-  def put(line_id, state) do
-    Agent.update(__MODULE__, &Map.put(&1, line_id, state))
+  def put(name, new_state) do
+    Agent.update(name, fn _ -> new_state end)
   end
 
-  def get(line_id) do
-    Agent.get(__MODULE__, fn map -> Map.get(map, line_id) end)
+  def get(name, line_id) do
+    Agent.get(name, fn map -> Map.get(map, line_id) end)
   end
 
-  def exists?(line_id) do
-    Agent.get(__MODULE__, fn map ->
-      IO.puts inspect map
-      Map.has_key?(map, line_id)
-    end)
+  def message(name, replyToken) do
+    unless Process.whereis(name) do
+      start_link(name)
+    end
+    {message, new_state} = case Agent.get(name, fn current_state -> current_state end) do
+      :worst -> {"週末何していますか？", :best}
+      :best  -> {"忙しいですか？", :try}
+      :try   -> {"救ってもらっていいですか？", :worst}
+      _      -> {"Error", :worst}
+    end
+
+    message
+    |> LinebotApi.reply_message(replyToken)
+
+    put(name, new_state)
   end
+
 end
